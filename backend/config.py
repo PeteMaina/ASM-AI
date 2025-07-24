c45e4dbcb31edc49d2ce46ff03891066df50af9d
Pete <peterwahomemaina003@gmail.com>
2025-07-24 10:09:20 +0300

    repackaged

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "your-very-secure-dev-key"
-    UPLOAD_FOLDER = "backend/docs"
-    MAX_CONTENT_LENGTH = 20 * 1024 * 1024  # 20MB file upload limit
